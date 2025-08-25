ad2a7e1068e74a7c1c9f50da200bdb23fb3a59fa
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2025-08-25 20:01:25 +0530

    to do list

---
+++ to-do-list/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [taskList, setList] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addtask = (e) => {
+    e.preventDefault();
+    if (task == "") return;
+    let tempList = taskList.slice();
+    tempList.push(task);
+    setList(tempList);
+    setTask("");
+  };
+
+  //Removing a task
+  const removeTask = (ind) => {
+    console.log(ind);
+
+    setList([...taskList.slice(0, ind), ...taskList.slice(ind + 1)]);
+  };
+  return (
+    <div className="main-page">
+      <div className="container">
+        <form
+          className="taskForm"
+          onSubmit={(e) => {
+            addtask(e);
+          }}
+        >
+          <input
+            type="text"
+            className="textArea"
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Add a task..."
+            value={task}
+          />
+          <button type="submit" className="addTask">
+            +
+          </button>
+        </form>
+            </div>
+        <div className="listt">
+          <ul>
+            {taskList.map((ele, ind) => {
+              return (
+                <li key={ind}>
+                  <span>{ele}</span>
+                  <button onClick={() => removeTask(ind)}>X</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+    </div>
+  );
+}
+
+export default App;
